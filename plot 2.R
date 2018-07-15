265e05410fbb6fe56776938731fc6be4068cfeaa
DwieshaE <38931457+DwieshaE@users.noreply.github.com>
2018-07-15 06:15:27 -0500

    Add files via upload

---
+++ plot 2.R
@@ -0,0 +1,37 @@
+setwd("/Users/Desktop/Datascience")
+install.packages("data.table")
+library("data.table")
+
+unzip('./exdata%2Fdata%2Fhousehold_power_consumption.zip')
+
+housedata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Format date to Type Date
+housedata$Date <- as.Date(housedata$Date, "%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+housedata <- subset(housedata,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+## Remove incomplete observation
+housedata <- housedata[complete.cases(t),]
+
+## Combine Date and Time column
+dateTime <- paste(housedata$Date, housedata$Time)
+
+## Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+## Remove Date and Time column
+housedata <- housedata[ ,!(names(housedata) %in% c("Date","Time"))]
+
+## Add DateTime column
+housedata <- cbind(dateTime, housedata)
+
+## Format dateTime Column
+housedata$dateTime <- as.POSIXct(dateTime)
+
+##PLOT 2
+## Create Plot 2
+plot(housedata$Global_active_power~housedata$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
